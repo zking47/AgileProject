5dab2108f6f82752318d4bdb88b24b1fd117bee7
Jordan Davis <jbd.profdev@gmail.com>
2023-10-17 12:06:15 -0400

    created the initial user models

---
+++ AgileProject.Models/User/UserOutbox.cs
@@ -0,0 +1,8 @@
+namespace AgileProject.Models.User
+{
+    public class UserOutbox
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ AgileProject.Models/User/UserSpam.cs
@@ -0,0 +1,8 @@
+namespace AgileProject.Models.User
+{
+    public class UserSpam
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
